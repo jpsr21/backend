ffb9f4918a6f5e0653c0fe9c9c6feb2d8907a62d
João Paulo <joaopaulosr21@gmail.com>
2019-11-06 11:02:40 -0300

    Primeiro commit do backend

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const routes = express.Router();
+
+const UserController = require("./controllers/UserController");
+
+//const User = mongoose.model('User');
+
+routes.get("/users", UserController.index);
+routes.get('/users/:id', UserController.show);
+routes.post("/users", UserController.store);
+routes.put("/users/:id", UserController.update);
+routes.delete("/users/:id", UserController.destroy);
+
+module.exports = routes;
